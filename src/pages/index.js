a34b1ec02ec5434d932c50705f0dcbe1c5d0a48a
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-07-31 14:27:38 +0100

    add matomo tracking
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,24 @@ import About from '@site/src/components/About'
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
+
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.ossupstream.org/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '6']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+
   return (
     <Layout
       title={`Welcome to the ${siteConfig.title} website!`}
       description="llm-d: a Kubernetes-native high-performance distributed LLM inference framework">
+
       <main>
         <Welcome />
         {/* <About /> */}
